02cb007ef2a03fac0a72696686ca13af6efac261
Cykablyat99 <50822619+JamessssLiu@users.noreply.github.com>
2019-11-29 11:30:20 +0800

    懒得统一格式乐

---
+++ RIE encoding/encode.py
@@ -0,0 +1,4 @@
+OriginCode=input("Please input the string")
+CodeLst=[]
+for char in OriginCode:
+    CodeLst.append(char)
---
+++ exercises/Ch1 image/2d array Image.py
@@ -0,0 +1,7 @@
+
+import numpy as np
+from PIL import image
+array=np.random.randint(0,10,(10,10))
+img=image.formarray(array)
+img.show()
+
